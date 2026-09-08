97f40ddc6114bf9918ae6008d8756898f2efaeae
Anto <anton.gonzalezc@duocuc.cl>
2026-09-08 20:32:55 -0300

    Update cliente.kt

--- app/lib/src/main/java/com/example/lib/cliente.kt
+++ app/lib/src/main/java/com/example/lib/cliente.kt
@@ -1,16 +1,13 @@
 package com.example.lib
 
-data class  Cliente (
-    val nombre: String,
-    val direccion: String
-)
+package com.example.lib
+import kotlinx.coroutines.*
 
-open class Producto(
-    val nombre: String,
-    val precioBase: Double
-){
+data class  Cliente (val nombre: String, val direccion: String, val esCliFrecuente: Boolean)
+
+open class Producto(val nombre: String, val precioBase: Double){
     open fun calcularPrecioFinal(): Double{7
-    return precioBase}
+        return precioBase}
 }
 
 class Plato(
@@ -22,11 +19,10 @@ class Plato(
     override fun calcularPrecioFinal(): Double{
         return if (tamanoPorcion == "Grande") {
             precioBase * 1.25
-    } else {
+        } else {
             precioBase
         }
     }
-        
 }
 
 class Bebida(
@@ -44,28 +40,30 @@ class Bebida(
     }
 }
 
+class pedido(val cliente: Cliente, val productos: List<Producto>, val horaPed:Int){
+    val total:Double= productos.sumOf { it.calcularPrecioFinal() }
+    val recargoNoc:Double= if(horaPed>=22 || horaPed<6) 1000.0 else 0.0
+    val calfiEnvioGratis:Boolean=total>=15000 && cliente.esCliFrecuente
 
-
-
-
-
-
+    fun calcularTotalFinal(): Double{
+        return total+recargoNoc
+    }
+}
 
 fun main(){
+    val pedProduc: List<Producto> = listOf(
+        Plato("Hamburguesa Doble Queso", 12000.0, "Grande"),
+        Plato("Papas Fritas", 5000.0, "Mediano"),
+        Bebida("Cerveza Ambar", 3500.0, esAlcoholica = true),
+        Bebida("Jugo Natural", 2500.0, esAlcoholica = false),
+        Bebida("Vino Tinto", 8000.0, esAlcoholica = true)
+    )
+    val bebidasAlcoholicas = pedProduc.filterIsInstance<Bebida>()
+        .filter{it.esAlcoholica}
+    val nomBebAlcoholicas = bebidasAlcoholicas.map{it.nombre}
+    val subTotal = pedProduc.sumOf{it.calcularPrecioFinal()}
 
+    println("Nombres de bebidas alcohólicas: $nomBebAlcoholicas")
+    println("Total del pedido: $$subTotal")
 
-
-
-
-    val horaPedido = 23
-    val esClienteFrecuente = true
-
-
-
-    val recargoNoturno = if (horaPedido >= 22 || horaPedido <6){
-        1000.0
-    } else {
-        0.0
-    }
-  
 }
