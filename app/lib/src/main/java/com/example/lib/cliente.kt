520534de58e98f660201b63217f5d1a12bb68144
incastellanos-cmd <in.castellanos@duocuc.cl>
2026-09-08 18:50:58 -0300

    Update cliente.kt

--- app/lib/src/main/java/com/example/lib/cliente.kt
+++ app/lib/src/main/java/com/example/lib/cliente.kt
@@ -1,3 +1,37 @@
 package com.example.lib
 
-data class cliente (val nom:String, val precioB:Double)
+ata class  Cliente (
+    val nombre: String,
+    val direccion: String
+)
+
+open class Producto(
+    val nombre: String,
+    val precioBase: Double
+){
+    open fun calcularPrecioFinal(): Double{7
+    return precioBase}
+}
+
+class Plato(
+    nombre: String,
+    precioBase: Double,
+    val tamanoPorcion: String
+) : Producto(nombre, precioBase){
+
+    override fun calcularPrecioFinal(): Double{
+        return precioBase * 1.25
+    }
+}
+
+
+
+
+
+
+
+
+
+fun main(){
+  
+}
